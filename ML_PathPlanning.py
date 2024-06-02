f93c977fc25e0357b31022262728546a9f516dd7
joshhlee173 <joshhlee@stanford.edu>
2024-06-01 17:51:21 -0700

    path works

--- ML_PathPlanning.py
+++ ML_PathPlanning.py
@@ -30,12 +30,12 @@ class DroneEnvironment:
         self.delivery = self.houses.copy()
         
         self.utility = np.zeros(gridsize)
-        self.utility[tuple(self.warehouse)] = -10
+        self.utility[tuple(self.warehouse)] = 0
         for i, h in enumerate(self.delivery):
             self.utility[tuple(h)] = 10
         for obstacle in self.obstacles:
             self.utility[tuple(obstacle)] = -100
-        print(self.utility)
+        # print(self.utility)
 
     def isdone(self):
         return np.all(self.delivered)
@@ -60,7 +60,7 @@ class DroneEnvironment:
         return self.get_state(), self.utility[tuple(future_state[0])], self.isdone()
 
     def possible_actions(self, pos):
-        possible_moves = np.array([[i, j, k] for i in [-1, 0, 1] for j in [-1, 0, 1] for k in [-1, 0, 1] if not (i == 0 and j == 0 and k == 0)])
+        possible_moves = np.array([[i, j, k] for i in [-1, 0, 1] for j in [-1, 0, 1] for k in [-1, 0, 1] ])
         valid_moves = []
         if pos == None:
             pos = self.drone.position
@@ -77,34 +77,42 @@ def value_iteration(k_max, drone_mdp, gamma):
     dim = np.array(drone_mdp.grid+(27,4))
     Qvalues = np.empty(dim)
     optimal_utility = np.copy(drone_mdp.utility)
+    alpha = 0.9
     for k_iter in range(k_max):
         print('Iteration: ',k_iter)
         for i in range(dim[0]):
             for j in range(dim[1]):
                 for k in range(dim[2]):
                     poss_a = drone_mdp.possible_actions((i,j,k))
+                    current_cost = optimal_utility[i,j,k]
                     for a in range(len(poss_a)):
-                        current_cost = optimal_utility[i,j,k]
                         next_step_cost = optimal_utility[i+poss_a[a][0], j+poss_a[a][1], k+poss_a[a][2]]
-                        Qvalues[i, j, k, a] = np.append(poss_a[a], (current_cost+gamma*next_step_cost))
+                        Qvalues[i, j, k, a] = np.append(poss_a[a], Qvalues[i,j,k,a,3] + alpha*(current_cost+gamma*next_step_cost))
                         q__ = Qvalues[i,j,k]
-                        optimal_utility[i,j,k] = np.argmax(q__[:,3])
+                        optimal_utility[i,j,k] = np.max(q__[:,3])
     return Qvalues, optimal_utility
 
 # Initialize the environment
 grid_size = (20, 20, 5)
-warehouse_pos = (10, 10, 0)
-houses_location = [(5, 5, 0), (19, 0, 0), (18, 3, 0)]
-obstacles = [(10, 8, 0), (8, 8, 0)]
+warehouse_pos = (0, 0, 0)
+houses_location = [(19, 19, 4)]
+
+obstacles = []
+
+for i in range(20):
+    for j in range(20):
+        for k in range(5):
+            # if k < 2:
+            #     obstacles.append((i,j,k))
+            if j < 7:
+                if i > 3:
+                    obstacles.append((i,j,k))
+            if j > 13:
+                if i < 17:
+                    obstacles.append((i,j,k))
 env = DroneEnvironment(grid_size, warehouse_pos, houses_location, obstacles)
 util = env.utility
-non_zero_indices = np.argwhere(util != 0)
-
-# Print the indices where util is not equal to 0
-print("Indices where util != 0:")
-for idx in non_zero_indices:
-    print(idx)
-Qvalues, optimal_q = value_iteration(10, env, 0.75)
+Qvalues, opt_util = value_iteration(50, env, 0.1)
 
 # Extract the Q-values for plotting
 x, y, z, q_values = [], [], [], []
@@ -112,15 +120,12 @@ for i in range(grid_size[0]):
     for j in range(grid_size[1]):
         for k in range(grid_size[2]):
             Q = Qvalues[i,j,k]
-            max_idx_q = np.argmax(Q[:,3])
-            opt_a_q = Q[max_idx_q, :3]
-            q_value = Q[max_idx_q,3]
-            
-            if q_value < 0 or q_value > 0:
+            Q = max(Q[:,3])
+            if  Q > 0:
                 x.append(i)
                 y.append(j)
                 z.append(k)
-                q_values.append(q_value)
+                q_values.append(Q)
 
 # Create a 3D scatter plot
 if x:
@@ -151,7 +156,6 @@ def simulate_optimal_path(Qvalues, warehouse_position):
         optimal_action = Q[max_idx_q, :3]
 
         # Calculate the next position based on the optimal action
-        # Calculate the next position based on the optimal action
         next_position = tuple(int(x) for x in np.array(current_position) + optimal_action)
 
         # Append the next position to the path
@@ -163,7 +167,6 @@ def simulate_optimal_path(Qvalues, warehouse_position):
 
         # Update the current position
         current_position = next_position
-
     return path
 
 # Simulate the optimal path starting from the warehouse position
